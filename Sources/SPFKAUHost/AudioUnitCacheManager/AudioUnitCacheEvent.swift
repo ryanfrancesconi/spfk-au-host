8c808cddb696b1f9e6b1d274272842ffcf43b7ee
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-03-10 09:38:26 -0700

    organizing AudioWorkspace Audio Unit support

--- Sources/SPFKAUHost/AudioUnitCacheManager/AudioUnitCacheEvent.swift
+++ Sources/SPFKAUHost/AudioUnitCacheManager/AudioUnitCacheEvent.swift
@@ -4,9 +4,13 @@ import Foundation
 /// Events emitted during audio unit cache discovery and validation.
 public enum AudioUnitCacheEvent: Sendable {
     case cachingStarted
-    case cacheUpdated
-    case cacheLoaded(SystemComponentsResponse)
 
     /// Progress update: name of the AU just validated, how many completed so far, total count
     case validating(name: String, completed: Int, count: Int)
+
+    /// The cache has be newly written
+    case cacheUpdated
+
+    /// Cache is loaded from disk
+    case cacheLoaded(SystemComponentsResponse)
 }

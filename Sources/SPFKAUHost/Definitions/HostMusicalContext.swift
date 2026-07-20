b954e7233c7c373ed8be20fbcee8ea9bbccf75fa
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-07-19 17:11:34 -0700

    Fix source file copyright headers to reference this package
    Headers were pointing at the wrong repo (often the package this one
    was split out of), missing the /ryanfrancesconi/<repo> revision-history
    URL entirely, or missing the header line altogether.

--- Sources/SPFKAUHost/Definitions/HostMusicalContext.swift
+++ Sources/SPFKAUHost/Definitions/HostMusicalContext.swift
@@ -1,3 +1,5 @@
+// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-au-host
+
 import Foundation
 
 /// Musical context provided to hosted Audio Units via `AUHostMusicalContextBlock`.
--- Sources/SPFKAUHost/Definitions/HostTransportState.swift
+++ Sources/SPFKAUHost/Definitions/HostTransportState.swift
@@ -1,3 +1,5 @@
+// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-au-host
+
 import AudioToolbox
 import Foundation
 
